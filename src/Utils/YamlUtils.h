be4f4040b75cadf4299bd9b6c0a9d768514d7859
Jason Heflinger <jasonheflinger@gmail.com>
2024-03-11 20:02:53 -0400

    used official c++ ignore warning instead of compiler gcc specific

--- src/Utils/YamlUtils.h
+++ src/Utils/YamlUtils.h
@@ -62,21 +62,21 @@ struct YAML::convert<glm::vec4> {
 	}
 };
 
-__attribute__((unused))
+[[maybe_unused]]
 static YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec2& v) {
 	out << YAML::Flow;
 	out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
 	return out;
 }
 
-__attribute__((unused))
+[[maybe_unused]]
 static YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& v) {
 	out << YAML::Flow;
 	out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
 	return out;
 }
 
-__attribute__((unused))
+[[maybe_unused]]
 static YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& v) {
 	out << YAML::Flow;
 	out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
