559df3d11b62e9962933b6dce919ef159718e265
Jason Heflinger <jasonheflinger@gmail.com>
2024-08-06 14:06:49 -0700

    linux suppression warning

--- src/Utils/YamlUtils.h
+++ src/Utils/YamlUtils.h
@@ -62,18 +62,27 @@ struct YAML::convert<glm::vec4> {
 	}
 };
 
+#ifdef __linux__
+[[maybe_unused]]
+#endif
 static YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec2& v) {
 	out << YAML::Flow;
 	out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
 	return out;
 }
 
+#ifdef __linux__
+[[maybe_unused]]
+#endif
 static YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& v) {
 	out << YAML::Flow;
 	out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
 	return out;
 }
 
+#ifdef __linux__
+[[maybe_unused]]
+#endif
 static YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& v) {
 	out << YAML::Flow;
 	out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
