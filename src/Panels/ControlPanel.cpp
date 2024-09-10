e56839f72157f94e68765c3c60e9814c53ed0c97
Jason Heflinger <jasonheflinger@gmail.com>
2024-09-10 11:31:16 -0400

    drafted out basic simulation settings in the ui

--- src/Panels/ControlPanel.cpp
+++ src/Panels/ControlPanel.cpp
@@ -16,7 +16,36 @@ void ControlPanel::Initialize() {
 }
 
 void ControlPanel::Update(Editor* context) {
-	ImGui::Text("Bello!");   
+	static uint64_t temp = 0;
+	static bool temp1 = false;
+    float gapsize = 8.0f;
+    ImGui::Columns(2);
+    ImGui::Dummy({0, gapsize});
+    ImGui::Text("Simulation Length");
+    ImGui::Dummy({0, gapsize});
+    ImGui::Text("Safeguard Cache");
+    ImGui::Dummy({0, gapsize});
+    ImGui::Text("Simulation Record");
+    ImGui::Dummy({0, gapsize});
+    ImGui::Text("Simulation Solver");
+    ImGui::Dummy({0, gapsize});
+    ImGui::NextColumn();
+    gapsize = 2.0f;
+    ImGui::Dummy({0, gapsize});
+	ImGui::DragScalar("##simulationlength", ImGuiDataType_U64, &temp, 1.0f);
+	ImGui::SameLine();
+	static int current_item_0 = 0;
+    const char* items_0[] = { "ticks", "us", "ms", "s" };
+    ImGui::Combo("##lengthunits", &current_item_0, items_0, IM_ARRAYSIZE(items_0));
+    ImGui::Dummy({0, gapsize});
+    ImGui::Checkbox("##safeguardcache", &temp1);
+    ImGui::Dummy({0, gapsize});
+    ImGui::Checkbox("##simulationrecord", &temp1);
+    ImGui::Dummy({0, gapsize});
+	static int current_item = 0;
+    const char* items[] = { "rkh45", "coolsolver", "???", "footbar" };
+    ImGui::Combo("##simulationsolver", &current_item, items, IM_ARRAYSIZE(items));
+
 	// Simulation length | █# of units█ █unit type dropdown█
 	// Safeguard Cache   | ■ Enabled
 	// Simulation Record | ■ Enabled
