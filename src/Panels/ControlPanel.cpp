a61d9361c6cd61fb3f044f1ef7ee0bafa7a29a86
Jason Heflinger <jasonheflinger@gmail.com>
2024-09-10 00:50:35 -0400

    added basic warning reminder and some planning comments

--- src/Panels/ControlPanel.cpp
+++ src/Panels/ControlPanel.cpp
@@ -1,11 +1,24 @@
 #include "ControlPanel.h"
 #include "Core/Editor.h"
+#include "Core/Log.h"
 #include "imgui.h"
 
 ControlPanel::ControlPanel() {
 	m_Name = "Simulation Properties";
 }
 
+void ControlPanel::Initialize() {
+	WARN("Currently there is no substantial implementation for"
+		"\n\t- Simulation length"
+		"\n\t- Simulation cache settings"
+		"\n\t- Simulation recording"
+		"\n\t- Simulation solver");
+}
+
 void ControlPanel::Update(Editor* context) {
 	ImGui::Text("Bello!");   
+	// Simulation length | █# of units█ █unit type dropdown█
+	// Safeguard Cache   | ■ Enabled
+	// Simulation Record | ■ Enabled
+	// Simulation Solver | █solver dropdown█
 }
--- src/Panels/ControlPanel.h
+++ src/Panels/ControlPanel.h
@@ -4,5 +4,6 @@
 class ControlPanel : public Panel {
 public:
 	ControlPanel();
+	virtual void Initialize() override;
 	virtual void Update(Editor* context) override;
 };
