b8c854775a8776216e8175818773da7c8953cec7
Jacob Consalvi <consaljj@rose-hulman.edu>
2024-10-18 10:00:44 -0400

    cleaned up display code and generated several 3 body orbits

--- prototyping/jacob_convert_electric_simulation/main.py
+++ prototyping/jacob_convert_electric_simulation/main.py
@@ -9,7 +9,7 @@ from time import time
 from AnimationWrapper import InteractiveAnimation, PLOT_POS
 
 
-def get_cmap(n, name="hsv"):
+def get_cmap(n, name="tab10"):
     """Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name."""
     return plt.get_cmap(name, n)
@@ -26,20 +26,58 @@ def get_cmap(n, name="hsv"):
 # mass = np.array([[5972000000000000000000000], [73476730900000000000000]])
 # state = np.array([[0, 0, 0, -12.5865197104], [384000000, 0, 0, 1023]])
 
-DT = 0.01  # timestep
+DT = 0.005  # timestep
 N = 3  # number of particles
-SIM_LEN = 2000  # number of steps to simulate
-SIM_SPEED = 3  # number of steps to skip in the animation
-E_PLOT_N = 50  # number of X and Y values in the electric field plot
+SIM_LEN = 2175  # number of steps to simulate
+SIM_SPEED = 12  # number of steps to skip in the animation
+E_PLOT_N = 100 # number of X and Y values in the electric field plot
 # N particles each have a state consisting of four values:
 # x position, y position, x velocity, y velocity
 state = np.zeros((N, 4))
 mass = np.array([[1], [1], [1]], dtype=np.float64)
+
+# Free Fall (Doesn't work for more than one "Orbit")
+# state = np.array(
+#     [
+#         [-0.5, 0, 0,0],
+#         [0.5, 0, 0,0],
+#         [0.0207067154,0.3133550361, 0,0],
+#     ]
+# )
+
+# 2.a Butterfly
+# state = np.array(
+#     [
+#         [-1, 0, 0.306893,0.125507],
+#         [1, 0, 0.306893,0.125507],
+#         [0, 0, -0.613786,-0.251014],
+#     ]
+# )
+
+# Ovals-With-Flourishes
+# state = np.array(
+#     [
+#         [0.716248295713,0.384288553041, 1.245268230896,2.444311951777],
+#         [0.086172594591,1.342795868577, -0.67522432369,-0.96287961363],
+#         [0.538777980808,0.481049882656, -0.570043907206,-1.481432338147],
+#     ]
+# )
+
+# Loop ended triangles.
+# state = np.array(
+#     [
+#         [0.6661637520772179,-0.081921852656887, 0.84120297540307,0.029746212757039],
+#         [-0.025192663684493022,0.45444857588251897, 0.142642469612081,-0.492315648524683],
+#         [-0.10301329374224,-0.765806200083609, -0.98384544501151, 0.462569435774018],
+#     ]
+# )
+
+# Broucke R 4
 state = np.array(
     [
-        [-1, 0, 0.3471128135672417, 0.532726851767674],
-        [1, 0, 0.3471128135672417, 0.532726851767674],
-        [0, 0, -0.69422562713, -1.06545370354],
+        [0.8733047091,0, 0,1.0107764436],
+        [-0.6254030288,0, 0,-1.6833533458],
+        [-0.2479016803,0, 0,0.6725769022],
     ]
 )
 
@@ -64,7 +102,7 @@ newcolors[np.where(newcolors < 0)] = 0
 cmap = colors.ListedColormap(newcolors)
 
 
-bound = 4
+bound = 2
 
 g_minmax_init = False
 g_field_min = 0
@@ -129,33 +167,44 @@ elapsed = end_time - start_time
 print(f"Finished cook'n. in {elapsed:.3f}s.")
 
 
-# plot initial particles and electric field
-fig = plt.figure()
-main_ax = fig.add_axes(PLOT_POS)
-main_ax.set_xlim(-bound, bound)
-main_ax.set_ylim(-bound, bound)
-main_ax.set_aspect("equal")
-field_range = 0
-# field_range = (g_field_max - g_field_min) * 0.1
-norm = colors.PowerNorm(
-    gamma=0.3, vmin=(g_field_min + field_range), vmax=(g_field_max - field_range)
-)
-mesh = main_ax.pcolormesh(X, Y, baked_g_fields[0], cmap="inferno", norm=norm)
-tmp = np.float64(mass / np.min(mass) + 1)
-scatter = main_ax.scatter(
-    state[:, 0], state[:, 1], s=np.log(tmp) * 15, c=mass, cmap=cmap, vmin=-3, vmax=3
-)
+def init_plots():
+    # plot initial particles and electric field
+    fig = plt.figure()
+    main_ax = fig.add_axes(PLOT_POS)
+    main_ax.set_xlim(-bound, bound)
+    main_ax.set_ylim(-bound, bound)
+    main_ax.set_aspect("equal")
+    field_range = 0
+    # field_range = (g_field_max - g_field_min) * 0.1
+    cmap = colors.ListedColormap(newcolors)
 
-cmap = get_cmap(N)
+    norm = colors.PowerNorm(
+        gamma=0.3, vmin=(g_field_min + field_range), vmax=(g_field_max - field_range)
+    )
+    mesh = main_ax.pcolormesh(X, Y, baked_g_fields[0], cmap="inferno", norm=norm)
+    tmp = np.float64(mass / np.min(mass) + 1)
+    scatter = main_ax.scatter(
+        state[:, 0], state[:, 1], s=np.log(tmp) * 15, c=mass, cmap=cmap, vmin=-3, vmax=3
+    )
 
-# divider = mpl_toolkits.axes_grid1.make_axes_locatable(main_ax)
-# trace_axis = divider.append_axes("right", size="100%", pad=0.05)
-# trace_axis.set_aspect("equal")
+    cmap = get_cmap(N)
 
-# for particle_num in range(N):
-#     px = simulation[:, particle_num, 0]
-#     py = simulation[:, particle_num, 1]
-#     trace_axis.scatter(px, py, s=0.05, color=cmap(particle_num))
+    divider = mpl_toolkits.axes_grid1.make_axes_locatable(main_ax)
+    trace_axis = divider.append_axes("right", size="100%", pad=0.05)
+    trace_axis.set_aspect("equal")
+
+
+    for particle_num in range(N):
+        px = simulation[:, particle_num, 0]
+        py = simulation[:, particle_num, 1]
+        trace_axis.scatter(px, py, s=0.1, color=cmap(particle_num))
+
+    return fig, mesh, scatter
+
+fig, mesh, scatter = init_plots()
+    # plt.show(block=False)
+
+
 
 
 def animate_func(i):
@@ -164,36 +213,48 @@ def animate_func(i):
     scatter.set_offsets(simulation[i * SIM_SPEED])  # update particles scatter plot
     return scatter, mesh
 
-
-# anim = InteractiveAnimation(
-#     0,
-#     SIM_LEN // SIM_SPEED,
-#     fig,
-#     animate_func,
-#     interval=67,
-#     save_count=SIM_LEN //SIM_SPEED,
-#     cache_frame_data=True,
-#     init_func=lambda : (scatter,mesh),
-# )
-
-anim = animation.FuncAnimation(
+print("Saving animation...")
+start_time = time()
+save_anim = animation.FuncAnimation(
     fig,
     animate_func,
     frames=range(SIM_LEN //SIM_SPEED),
     interval=10,
 )
+save_anim.save("./animation.gif", fps=30)  # dpi=50 for lower quality to reduce file size
+print(f"Saved animation in {time()-start_time:.2f}s")
+
+del save_anim
+plt.clf()
+plt.close()
+fig, mesh, scatter = init_plots()
+
+anim = InteractiveAnimation(
+    0,
+    SIM_LEN // SIM_SPEED,
+    fig,
+    animate_func,
+    interval=67,
+    save_count=SIM_LEN //SIM_SPEED,
+    cache_frame_data=True,
+    init_func=lambda : (scatter,mesh),
+)
+
+
 
 # fig.set_size_inches(6, 6)
 # fig.subplots_adjust(
 #     left=0, bottom=0, right=1, top=1, wspace=None, hspace=None
 # )  # remove white border
 # plt.axis("off")
-
+# plt.draw()
 plt.show()
+del anim
+plt.clf()
+plt.close()
 
 # --------------- PLOT ENERGY ------------------
 
-fig = plt.figure()
 print("Calculating energy...")
 ke, pe, total_e = calc_energy()
 fake_time = [x for x in range(len(ke))]
@@ -207,9 +268,9 @@ ax2.set_title("Total Potential Energy")
 
 ax3.plot(fake_time, total_e)
 ax3.set_title("Total Energy")
-print(total_e[-1])
-print(total_e[0])
-print(total_e[-1] - total_e[0])
+print(max(total_e))
+print(min(total_e))
+print(max(total_e) - min(total_e))
 plt.show()
 
 
@@ -230,6 +291,3 @@ plt.show()
 # ax.set_aspect("equal")
 # plt.show()
 
-
-# save_anim = animation.FuncAnimation(fig, animate_func, interval=67)
-# save_anim.save("./animation.gif", dpi=40)  # dpi=50 for lower quality to reduce file size
